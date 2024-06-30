37cae353daaef4bfcc6121cb609341425448e5eb
lucja001 <wwx22352@student.warszawa.merito.pl>
2024-06-30 23:57:40 +0200

    hello world 3

---
+++ new-feature.py
@@ -0,0 +1,10 @@
+public class NewFeature {
+    public static void newFunction() {
+        System.out.println("This is a new feature.");
+    }
+
+    public static void main(String[] args) {
+        // Wywołanie metody newFunction i wypisanie wyniku na ekran
+        newFunction();
+    }
+}
